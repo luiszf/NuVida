e1c98c62bea7c1b95b43ac5b5967e69910bfd61c
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-08-12 20:08:45 -0300

    the first answer it's ok

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,4 +1,3 @@
-function initChatbot() {
   function clientMsg() {
     const inputMsg = document.querySelector(".input-msg");
     const buttonSend = document.querySelector(".btn-send");
@@ -6,16 +5,41 @@ function initChatbot() {
 
     function toSendMsg() {
       const clientMsg = document.createElement("div");
-
       clientMsg.innerText = inputMsg.value;
       clientMsg.classList.add("client-msg", "every-box-msg");
       containerSentMsg.appendChild(clientMsg);
+      companieMsg();
     }
-
     buttonSend.addEventListener("click", toSendMsg);
   }
   clientMsg();
+
+  function companieMsg(){
+    const containerSentMsg = document.querySelector(".container-conversation");
+
+    function toSendMsg() {
+      const companieMsg = document.createElement("div");
+      companieMsg.innerText = msgs[1];
+      companieMsg.classList.add("companie-msg", "every-box-msg");
+      containerSentMsg.appendChild(companieMsg);
+    }
+    toSendMsg();
   }
 
-initChatbot();
-
+  const msgs = {
+    1: `Boa noite!
+    Por favor, escolha o sabor de sua pizza:
+    [1] - Mussarela
+    [2] - Calabresa
+    [3] - Bauru
+    [4] - Carne seca
+    [5] - Bacon`,
+    2: `Vai querer mais uma?`,
+    3: `Ótimo! Gostaria de beber alguma coisa?`,
+    4: `Por favor, escolha a bebida:
+    [1] - Dolly
+    [2] - Coca-cola
+    [3] - Fanta uva
+    [4] - Fanta laranja
+    [5] - Guaraná Jesus`,
+  }
